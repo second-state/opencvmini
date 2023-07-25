b3276494f4308564eab68253392bb833fbe95d78
Lîm Tsú-thuàn <dannypsnl@secondstate.io>
2023-07-25 13:03:13 +0800

    complete forwarding
    Signed-off-by: Lîm Tsú-thuàn <dannypsnl@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ type MatKey = generated::mat_key;
 pub fn imdecode(buf: &[u8]) -> MatKey {
     unsafe { generated::imdecode(buf.as_ptr(), buf.len()) }
 }
-
 /// # Example
 ///
 /// ```rust
@@ -21,4 +20,25 @@ pub fn imencode(ext: &str, m: MatKey, buf: &[u8]) {
     unsafe {
         generated::imencode(ext.as_ptr(), ext.len(), m, buf.as_ptr(), buf.len());
     }
+}
+pub fn imwrite(file_name: &str, m: MatKey) {
+    unsafe {
+        generated::imwrite(file_name.as_ptr(), file_name.len(), m);
+    }
+}
+pub fn imshow(window_name: &str, m: MatKey) {
+    unsafe { generated::imshow(window_name.as_ptr(), window_name.len(), m) }
+}
+pub fn waitkey(delay: u32) {
+    unsafe { generated::waitkey(delay) }
+}
+
+pub fn blur(m: MatKey, kernel_width: u32, kernel_height: u32) -> MatKey {
+    unsafe { generated::blur(m, kernel_width, kernel_height) }
+}
+pub fn normalize(m: MatKey) -> MatKey {
+    unsafe { generated::normalize(m) }
+}
+pub fn bilinear_sampling(m: MatKey, w: u32, h: u32) -> MatKey {
+    unsafe { generated::bilinear_sampling(m, w, h) }
 }
